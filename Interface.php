9e2ea3771fab748d21a2ff4ad2b8ee5d47233bc4
Dustin Dinh <djdinh@gmail.com>
2015-05-07 14:24:58 -0700

    Started Assignment

---
+++ Interface.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>login.php</title>
+</head>
+<body>
+<h1>Login Page</h1>
+<section>		
+	<form action="content1.php" method="post">
+		<legend>Human, enter your slave name</legend>
+	    UserName:  <input type="text" name="username" /><br />
+    	<input type="submit" name = "Login"/>
+	</form>
+
+</section>
+<?php
+
+?>
+
+</body>
+</html>
